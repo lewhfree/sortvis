7cef7ee4598a1c9466035daf97af8cd8278ee3f9
lewhfree <>
2025-04-29 15:11:09 -0700

    Added basic files and sorter skeletons

---
+++ main.py
@@ -0,0 +1,6 @@
+#Use tkinter to do a visualization
+import sorters
+
+array = [3, 4, 5, 1, 2, 5, 7, 6, 8]
+
+sorter = new Bubble(array)
---
+++ sorters.py
@@ -0,0 +1,15 @@
+class Bubble:
+    def __init__(self, toSort):
+        self.TOSORT = toSort
+        self.used   = self.TOSORT
+
+    def step():
+        self.used = self.used
+
+class QSort:
+    def __init__(self, toSort):
+        self.TOSORT = toSort
+        self.used   = self.TOSORT
+
+    def step():
+        self.used = self.used
